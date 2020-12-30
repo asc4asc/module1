dc7fe90200cdae885fb6541c0c75241520812f42
Andreas <asc4asc@users.noreply.github.com>
2020-12-30 15:33:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as c1 from './lib/counter.js';
   
-console.log("Info1:",c1.count); // 0
+console.log("Info1:",count); // 0
 c1.increment();
-console.log("Info2:",c1.count); // 1
+console.log("Info2:",count); // 1
 c1.decrement();  
 c1.decrement();  
-console.log("Info3:",c1.count); // -1  
+console.log("Info3:",count); // -1  
